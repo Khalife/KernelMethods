da86fda3ec734b9c7d197e639384abc889944dd8
Khalife <sam_411@hotmail.fr>
2015-02-04 11:28:37 +0100

    newChanges

--- kernelTrickDenoising.m
+++ kernelTrickDenoising.m
@@ -1,4 +1,4 @@
-filename='train.0';
+filename='train.3';
 data=load(filename);
 n=100;
 X=data([1:n],:);
@@ -19,11 +19,7 @@ G_centered=(eye(n)-U)*G*(eye(n)-U);
 [V,D] = eig(G_centered);
 [EigenValuesOrdered,IndexEigen]= sort(diag(D),'descend');
 
-
-%d_set=[1:50:1000];
-%error=zeros(size(d_set,2),1);
-%for d_=1:size(d_set,2)
-%d=d_set(d_); %number of eigenvectors taken in account
+%% Denoising algorithm with d=50
 d=50;
 FirstEigenVectors = V(:,IndexEigen(1:d));
 
@@ -39,7 +35,7 @@ end
 % Computing gammas
 gamma=zeros(n,1);
 sigma_noise=0.5;
-X_test=data(1001,:);
+X_test=data(500,:);
 X_test_noised=X_test+(1-2*randi(1,1))*(sigma_noise*randn(1,p)); %add noise 
 K_temp=zeros(n,1);
 
@@ -59,14 +55,11 @@ for j=1:n
     
 end
 
-
 % Fixed point method
-%y=rand(1,p);
 y=-1+2*rand(1,p);
 while (1)
 
 z=y;
-
 for i=1:p
     y(i)=sum(X(:,i).*exp(-sum((repmat(z,n,1)-X).^2,2)/2*sigma^2),1);    
 end
@@ -77,46 +70,91 @@ end
 %norm(z-y)
 end
 
+K_error=zeros(n,1);
+for i=1:n
+    K_error(i)=GaussianKernel(X(i,:),y);
+end
 
-%error(d_)=norm(X_test_noised-y);
 subplot(2,2,1), imshow(reshape(X_test_noised,16,16),[min(X_test_noised(:)) max(X_test_noised(:))]), title('Noised data')
 subplot(2,2,2), imshow(reshape(y,16,16),[min(y(:)) max(y(:))]), title('Denoised')
 subplot(2,2,3), imshow(reshape(X_test,16,16),[min(X_test(:)) max(X_test(:))]), title('Original data')
-d
-%end
+
+
+
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%% Loop on d %%%%%%%%%%%%%%%%%%%%%
+
+% d_set=[1:100];
+% error=zeros(size(d_set,2),1);
+% for d_=1:size(d_set,2)
+% d=d_set(d_); %number of eigenvectors taken in account
+% d=50;
+% FirstEigenVectors = V(:,IndexEigen(1:d));
+% 
+% %compute the alpha_i s
+% alpha = zeros(n,d);
+% 
+% for i=1:d
+%     % normalize eigenvector
+%     alpha(:,i) = FirstEigenVectors(:,i) / sqrt(EigenValuesOrdered(i));
+%     alpha(:,i) = alpha(:,i) - mean(alpha(:,i));
+% end
+% 
+% % Computing gammas
+% gamma=zeros(n,1);
+% sigma_noise=0.5;
+% X_test=data(1001,:);
+% X_test_noised=X_test+(1-2*randi(1,1))*(sigma_noise*randn(1,p)); %add noise 
+% K_temp=zeros(n,1);
+% 
+% for i=1:n
+%     K_temp(i)=GaussianKernel(X_test_noised,X(i,:));%-(1/n)*sum(G(i,:),2);
+% end
+% 
+% for j=1:n
+%     gamma(j)=1/n;%+sum(K_temp.*sum(FirstEigenVectors.*repmat(FirstEigenVectors(j,:),n,1),2),1);
+%     coeff = 0;
+%     for u = 1:n
+%         for i = 1:d
+%             coeff = coeff + alpha(u,i)*alpha(j,i);
+%         end
+%         gamma(j) = gamma(j) + (K_temp(u) - mean(G(:,u))) * coeff;
+%     end
+%     
+% end
+% 
+% % Fixed point method
+% y=-1+2*rand(1,p);
+% while (1)
+% 
+% z=y;
+% for i=1:p
+%     y(i)=sum(X(:,i).*exp(-sum((repmat(z,n,1)-X).^2,2)/2*sigma^2),1);    
+% end
+% y=y/sum(exp(-sum((repmat(z,n,1)-X).^2,2)/2*sigma^2),1);
+% if  norm(z-y) < 1e-3
+%     break
+% end
+% %norm(z-y)
+% end
+% 
+% K_error=zeros(n,1);
+% for i=1:n
+%     K_error(i)=GaussianKernel(X(i,:),y);
+% end
+% 
+% error(d_)=1-2*sum(gamma.*K_error)+dot(gamma,G*gamma);
+% d_
+% end
+% 
+% plot(error)
+% title('Error = f(d)')
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 %Gradient method (not implemented yet)
 
-% while (1)
-%  
-% d_  = 1./(b-A*x(:,end));
-% d  = A'*d_;            % Gradient
-% H  = A'*diag(d_.^2)*A; % Hessian    
-% 
-% delta = H\d;
-% lambda=dot(d,H*d);
-% error=[error lambda];
-% 
-% if (lambda < 2*epsilon || compteur > max_iter-1)
-%     break
-% end
-% 
-% t_step=1;
-% 
-% 
-% while (1)
-% 
-%     
-%     if ( isempty(find(A*(x-t_step*delta)-b >= 0) ) ) % condition for feasible point
-%             q1 = eval_barrier_objective(x-t_step*delta,A,b);
-%             q2 = eval_barrier_objective(x,A,b) - alpha*t_step*dot(d,delta);
-%         if (q1 < q2 || t_step*norm(delta)==0 )
-%             break
-%         end
-%     t_step=beta*t_step;
-%     end
-% end
-% x=x-t_step*delta;
-% trajectory=[trajectory,x];
-% compteur=compteur+1;
-% end
+
